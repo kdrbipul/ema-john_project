2d2edb50d9153c9d67b4c7d22a70773701a417a2
kdrbipul <kdrbipul@gmail.com>
2023-09-28 13:42:40 +0600

    home part clear

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import profile from '../../assets/images/austin-wade-AoYT0ArTTmg-unsplash 1.png'
+import './Home.css'
 const Home = () => {
     return (
         <>
-            
+            <div className='container home_part'>
+                <div className='content'>
+                    <p>Sale up to 70% off</p>
+                    <h1>New Collection For Fall</h1>
+                    <h4>Discover the all new arrivals of ready-to-wear collection.</h4>
+                    <Link to='/order'><button type='submit' className='btn btn-outline-warning'>Shope Now</button></Link>
+                </div>
+                <div className='profile_picture'>
+                    <div className="profile_picture-inner">
+
+                    </div>
+                    <div className="profile_picture-outer">
+                        <img src={profile} alt="" />
+                    </div>
+                </div>
+            </div>
         </>
     );
 };
--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,10 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
+import Home from "../Components/Home/Home";
 import Main from "../Layout/Main";
 
 const router = createBrowserRouter([
     {
         path:'/',
-        element:<Main />
+        element:<Main />,
+        children:[
+            {
+                path:'/',
+                element:<Home />
+            }
+        ]
     }
 ])
 export default router;
