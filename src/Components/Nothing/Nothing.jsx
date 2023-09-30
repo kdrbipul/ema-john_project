8d24f73a6ec2650f16bf5fe4a898990b626d71a0
kdrbipul <kdrbipul@gmail.com>
2023-09-30 11:52:48 +0600

    some correction

--- src/Components/Nothing/Nothing.jsx
+++ src/Components/Nothing/Nothing.jsx
@@ -4,7 +4,7 @@ const Nothing = () => {
     return (
         <div className='container'>
             
-            <h1 className='d-flex justify-content-center align-items-center my-5 py-5 rounded-2 bg-warning text-danger'>404 <span>This is a not a valid way</span></h1>
+            <h1 className='d-flex justify-content-center align-items-center my-5 py-5 rounded-2 bg-warning text-danger'>404 <span>This is not a valid way</span></h1>
         </div>
     );
 };
