f343ae06b5e3bdca7d94f891e98c1eb0a1fead77
kdrbipul <kdrbipul@gmail.com>
2023-10-01 04:55:39 +0600

    last commit of taday

--- src/UserLogin/Registration/Rsgister.jsx
+++ src/UserLogin/Registration/Rsgister.jsx
@@ -1,20 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FcGoogle } from 'react-icons/fc';
 import { ImGithub } from 'react-icons/im';
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import "./Register.css"
+import app from './../../firebase/firebase.config';
 
-const auth = getAuth();
+const auth = getAuth(app);
 
 const Rsgister = () => {
     const googleProvider = new GoogleAuthProvider();
+    const [user, setUser] = useState({});
 
-    const handleGoogleLogin = () =>{
+    const handleGoogleLogin = (e) =>{
+        e.preventDefault();
+        console.log("Clicked the button");
         signInWithPopup(auth,googleProvider)
         .then((result)=>{
             const user = result.user;
             console.log(user);
+            setUser(user)
+        }).catch((error)=>{
+            "Firebase error", error;
         })
     }
 
@@ -31,31 +38,31 @@ const Rsgister = () => {
     return (
         <>
        <div className="container">
-                
+                <div user={user}></div>
                 <div className="m_card">
                     <form className='s_form' action="">
                     <h1>Sign In</h1>
                         <div className="m_card_content">
                             <label for="name" className='m_card_content-text'>Name : </label><br />
-                            <input type="text" id="name" name="name" placeholder="Enter your email address" required />
+                            <input type="text" id="name" name="name" placeholder="Enter your email address"  />
                         </div>
                         <div className="m_card_content">
                             <label for="name" className='m_card_content-text'>Email : </label><br />
-                            <input type="text" id="name" name="email" placeholder="Enter your email address" required />
+                            <input type="text" id="name" name="email" placeholder="Enter your email address"  />
                         </div>
                         <div className="m_card_content">
                             <label for="name" className='m_card_content-text'>Password : </label><br />
-                            <input type="password" id="myInput" name="password" placeholder="Enter your password" required />
+                            <input type="password" id="myInput" name="password" placeholder="Enter your password"  />
                         </div>
                         <div>
-                            <input type="checkbox" onClick={()=>myFunction()}   required />Show Password
+                            <input type="checkbox" onClick={()=>myFunction()}    />Show Password
                             <label for="name"></label>
                         </div>
                         <div>
                             <button className='btn btn-danger w-100 py-2 my-2'>Sign In</button>
                         </div>
                         <div>
-                            <button className='btn btn-danger w-100 py-2 my-2'><span className='register_icon' onClick={handleGoogleLogin}><FcGoogle></FcGoogle></span>Sign In with Google</button>
+                            <button className='btn btn-danger w-100 py-2 my-2'><span className='register_icon' onClick={()=>handleGoogleLogin()}><FcGoogle></FcGoogle></span>Sign In with Google</button>
                         </div>
                         <div>
                             <button className='btn btn-danger w-100 py-2 my-2'><span className='register_icon'><ImGithub></ImGithub></span>Sign In with GitHub</button>
